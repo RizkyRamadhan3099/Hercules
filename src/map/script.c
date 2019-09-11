71c74c7321b9462d0e246261f41d898b6822e99c
skyleo <skyleo@skyleo.de>
2019-09-12 00:33:54 +0200

    Change returning error-code in unit->walktoxy to obey code-style guidelines

--- src/map/script.c
+++ src/map/script.c
@@ -19532,7 +19532,7 @@ static BUILDIN(setunitdata)
 			unit->warp(bl, (short) val, (short) val2, (short) val3, CLR_TELEPORT);
 			break;
 		case UDT_WALKTOXY:
-			if (!unit->walktoxy(bl, (short) val, (short) val2, 2))
+			if (unit->walktoxy(bl, (short)val, (short)val2, 2) != 0)
 				unit->movepos(bl, (short) val, (short) val2, 0, 0);
 			break;
 		case UDT_SPEED:
@@ -19703,7 +19703,7 @@ static BUILDIN(setunitdata)
 			unit->warp(bl, (short) val, (short) val2, (short) val3, CLR_TELEPORT);
 			break;
 		case UDT_WALKTOXY:
-			if (!unit->walktoxy(bl, (short) val, (short) val2, 2))
+			if (unit->walktoxy(bl, (short)val, (short)val2, 2) != 0)
 				unit->movepos(bl, (short) val, (short) val2, 0, 0);
 			break;
 		case UDT_SPEED:
@@ -19842,7 +19842,7 @@ static BUILDIN(setunitdata)
 			unit->warp(bl, (short) val, (short) val2, (short) val3, CLR_TELEPORT);
 			break;
 		case UDT_WALKTOXY:
-			if (!unit->walktoxy(bl, (short) val, (short) val2, 2))
+			if (unit->walktoxy(bl, (short)val, (short)val2, 2) != 0)
 				unit->movepos(bl, (short) val, (short) val2, 0, 0);
 			break;
 		case UDT_SPEED:
@@ -19975,7 +19975,7 @@ static BUILDIN(setunitdata)
 			unit->warp(bl, (short) val, (short) val2, (short) val3, CLR_TELEPORT);
 			break;
 		case UDT_WALKTOXY:
-			if (!unit->walktoxy(bl, (short) val, (short) val2, 2))
+			if (unit->walktoxy(bl, (short)val, (short)val2, 2) != 0)
 				unit->movepos(bl, (short) val, (short) val2, 0, 0);
 			break;
 		case UDT_SPEED:
@@ -20109,7 +20109,7 @@ static BUILDIN(setunitdata)
 			unit->warp(bl, (short) val, (short) val2, (short) val3, CLR_TELEPORT);
 			break;
 		case UDT_WALKTOXY:
-			if (!unit->walktoxy(bl, (short) val, (short) val2, 2))
+			if (unit->walktoxy(bl, (short)val, (short)val2, 2) != 0)
 				unit->movepos(bl, (short) val, (short) val2, 0, 0);
 			break;
 		case UDT_SPEED:
@@ -20238,7 +20238,7 @@ static BUILDIN(setunitdata)
 			unit->warp(bl, (short) val, (short) val2, (short) val3, CLR_TELEPORT);
 			break;
 		case UDT_WALKTOXY:
-			if (!unit->walktoxy(bl, (short) val, (short) val2, 2))
+			if (unit->walktoxy(bl, (short)val, (short)val2, 2) != 0)
 				unit->movepos(bl, (short) val, (short) val2, 0, 0);
 			break;
 		case UDT_CLASS:
@@ -20973,7 +20973,10 @@ static BUILDIN(unitwalk)
 	if (script_hasdata(st, 4)) {
 		int x = script_getnum(st, 3);
 		int y = script_getnum(st, 4);
-		script_pushint(st, unit->walktoxy(bl, x, y, 0));// We'll use harder calculations.
+		if (unit->walktoxy(bl, x, y, 0) == 0) // We'll use harder calculations.
+			script_pushint(st, 1);
+		else
+			script_pushint(st, 0);
 	}
 	else {
 		int target_id = script_getnum(st, 3);
